ec3c9ab4776c807a20885027eab4bf01bedbbc2d
mayconds000 <mayconds000@gmail.com>
2018-03-20 22:05:40 -0300

    Add a abstract controller

--- server/app/Http/Controllers/Controller.php
+++ server/app/Http/Controllers/Controller.php
@@ -3,8 +3,81 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use App\Repositories\Contracts\RepositoryInterface;
+use Illuminate\Http\Request;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
-    //
+    protected $repository;
+
+    abstract protected function validateCreate(Request $request);
+    abstract protected function validateUpdate(Request $request);
+
+    public function all() {
+        return response()->json($this->repository->all());
+    }
+
+    public function create(Request $request) {
+        $validator = $this->validateCreate($request);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            return response()->json([
+                "status" => 400,
+                "errors" => ["fields" => $errors],
+            ], 400);
+        }
+
+        if ($model = $this->repository->create($request->all())) {
+            return response()->json($model, 200);
+        }
+
+        return response()->json([
+            "status" => 422,
+            "errors" => ["Houve uma falha ao cadastrar"]
+        ], 422);
+    }
+
+    public function find($id) {
+        if ($model = $this->repository->find($id)) {
+            return response()->json($model, 200);
+        }
+
+        return response()->json([
+            "status" => 404,
+            "errors" => ["Registro não encontrado"]
+        ], 400);
+    }
+
+    public function update(Request $request, $id) {
+        $validator = $this->validateUpdate($request);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            return response()->json([
+                "status" => 400,
+                "errors" => ["fields" => $errors],
+            ], 400);
+        }
+
+        if ($response = $this->repository->update($request->all(), $id)) {
+            return response()->json($response, 200);
+        }
+
+        return response()->json([
+            "status" => 422,
+            "errors" => ["Não foi possível atualizar o registro"]
+        ], 422);
+    }
+
+    public function delete($id) {
+        if ($response = $this->repository->delete($id)) {
+            return response()->json($response, 200);
+        }
+
+        return response()->json([
+            "status" => 422,
+            "errors" => ["Não foi possível deletar o registro"]
+        ], 422);
+    }
 }
--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -6,89 +6,28 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Repositories\UserRepository as User;
 
-
-
 class UserController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    private $user;
+    protected $repository;
     
     public function __construct(User $user)
     {
-        $this->user = $user;
+        $this->repository = $user;
     }
 
-    public function all() {
-        return response()->json($this->user->all());
-    }
-
-    public function create(Request $request) {
-        $validator = Validator::make($request->all(), [
+    protected function validateCreate(Request $request) {
+        return Validator::make($request->all(), [
             'name' => 'required|max:255',
             'email' => 'required|unique:users',
-            'password' => 'required|min:6'    
+            'password' => 'required|min:6'
         ]);
-
-        if ($validator->fails()) {
-            $errors = $validator->errors()->toArray();
-            return response()->json([
-                "error" => ["errors" => $errors],
-                "status" => 500,
-                "message" => "Bad Request"
-            ], 500);
-        }
-
-        $data = $request->only(['name', 'email', 'password']);
-        $data['password'] = app('hash')->make($data['password']);
-
-        if ($user = $this->user->create($data)) {
-            return response()->json($user, 200);
-        }
-
-        return response()->json([
-            "error" => ["errors" => ["Não foi possível criar o usuário."]],
-            "status" => 500,
-            "message" => "Não foi possível gravar o usuário no banco de dados",
-        ], 500);
-    }
-    
-    public function find($id) {
-        $user = $this->user->find($id);
-        if ($user) { return response()->json($user, 200); }
-        return response()->json([
-            "status" => 400,
-            "message" => "Bad request",
-            "error" => ["errors" => ["Usuário não encontrado"]]
-        ], 400);
     }
 
-    public function update(Request $request, $id) {
-        $res = $this->user->update($request->all(), $id);
-        if (! $res) {
-            return response()->json([
-                "status" => 400,
-                "message" => "Bad request",
-                "error" => ["errors" => ["Usuário não encontrado"]]
-            ], 400);
-        }
-
-        return response()->json([
-            'status' => 200,
-            'user' => $this->user->find($id)
-        ], 200);
-    }
-
-    public function delete($id) {
-        $res = $this->user->delete($id);
-        if ($res) { 
-            return response()->json([
-                "status" => 200,
-                "message" => "ok"
-            ], 200);
-        }
+    protected function validateUpdate(Request $request) {
+        return Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users',
+            'password' => 'min:6'
+        ]);
     }
 }
